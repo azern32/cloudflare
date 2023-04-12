d7e6d3154fbb2f7a53fcd1c298781973a0350e1a
azern32 <chiro.emiya@gmail.com>
2023-04-12 15:42:25 +0800

    wah

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,6 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-cloudflare';
+
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
